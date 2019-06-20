71d6e395972e7e0ede4d44f944f9ca28af61575e
Matt Galvin <matt.galvin@dockayrd.com>
2019-06-21 01:33:57 +0200

    Hash password, update user changeset to update user and credential

--- lib/our_wedding/accounts/user.ex
+++ lib/our_wedding/accounts/user.ex
@@ -6,8 +6,15 @@ defmodule OurWedding.Accounts.User do
     field :first_name, :string
     field :last_name, :string
     field :telephone_number, :integer
+    has_one :credential, Credential
 
     timestamps()
+  end
+
+  def registration_changeset(user, params) do
+    user
+    |> changeset(params)
+    |> cast_assoc(:credential, with: &Credential.changeset/2, required: true)
   end
 
   def changeset(user, attrs) do
