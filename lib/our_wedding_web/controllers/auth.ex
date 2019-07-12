5f2955c5715a5e5c95380737372fcdecd442c2bc
Matt Galvin <matt.galvin@dockayrd.com>
2019-07-12 14:55:05 -0400

    Add auth plug, user index and show only available to auth'd users

---
+++ lib/our_wedding_web/controllers/auth.ex
@@ -0,0 +1,11 @@
+defmodule OurWedding.Auth do
+  import Plug.Conn
+
+  def init(opts), do: opts
+
+  def call(conn, _opts) do
+    user_id = get_session(conn, :user_id)
+    user = user_id && OurWedding.Accounts.get_user(user_id)
+    assign(conn, :current_user, user)
+  end
+end
--- lib/our_wedding_web/router.ex
+++ lib/our_wedding_web/router.ex
@@ -7,6 +7,7 @@ defmodule OurWeddingWeb.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug OurWedding.Auth
   end
 
   pipeline :api do
